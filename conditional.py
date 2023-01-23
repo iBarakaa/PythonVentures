24c21fb1ab2312928b50031a3007220d1057b807
iBarakaa <isaacbaraka1@gmail.com>
2023-01-23 18:56:19 +0300

    Intro to conditional execution

---
+++ conditional.py
@@ -0,0 +1,11 @@
+#flowcharts aid in the understanding of conditional statements
+x = 0
+y = 10
+
+if 0 == x:
+    if y == 10:
+        print('Yes')
+    else:
+        print('Damn son')
+else:
+    print('Damn son')
