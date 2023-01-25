d057146a56cad7462f398baa97630983fcd0c621
iBarakaa <isaacbaraka1@gmail.com>
2023-01-25 21:09:53 +0300

    Intro to Python functions

---
+++ functions.py
@@ -0,0 +1,7 @@
+#def keyword stores a piece of code (create functions)
+#print() is a function for instance
+# Hence, there are two types of functions, Built-in functions and Programmer-Defined function
+
+ #instance of max() function - prints largest character in string
+big = max('Hello world')
+print(big) #'w' is printed as it is the largest character in the string
