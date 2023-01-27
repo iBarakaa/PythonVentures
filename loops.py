405f5693402d477107e746f1021b97fc8c985fff
iBarakaa <isaacbaraka1@gmail.com>
2023-01-27 22:30:59 +0300

    Infinite loop illustration

--- loops.py
+++ loops.py
@@ -5,3 +5,12 @@ while n > 0:
     n = n - 1 
 print('Blastoff!')
 print(n)
+
+#infinite loop
+# n = 5
+# while n > 0:
+#     print('Lather')
+#     print('Rinse')
+# print('Dry off')
+#uncomment the code to see the behaviour of an infinite loop
+
