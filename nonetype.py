44c858093609e0e076b45d5bd4e6a48983405475
iBarakaa <isaacbaraka1@gmail.com>
2023-02-02 11:52:00 +0300

    None type solution using if-else statement

--- nonetype.py
+++
@@ -1,9 +0,0 @@
-import re
-
-# search for the upper case character in word beginning and print the word
-text = "I am the Last Dragon God Superman Sir"
-
-# the initial instance of this code produces an error
-for i in text.split():
-    x = re.match(r"\bS\w+", i)
-    print(x.group())
---
+++ supplementary_knowledge/nonetypesolns.py
@@ -0,0 +1,6 @@
+# use of the if else blocks and the use of the try and except blocks
+x = None
+if x is not None:
+    x.something = 'Finxter'
+else:
+    print('x is a None type object')
