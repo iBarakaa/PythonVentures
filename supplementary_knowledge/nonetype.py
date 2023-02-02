ac5b43203b3273f5d78cec5defb83f10dccd9e2f
iBarakaa <isaacbaraka1@gmail.com>
2023-02-02 11:47:56 +0300

    None type error generating codebloc

--- supplementary_knowledge/nonetype.py
+++ supplementary_knowledge/nonetype.py
@@ -3,6 +3,8 @@ import re
 # search for the upper case character in word beginning and print the word
 text = "I am the Last Dragon God Superman Sir"
 
+# the different codeblocs result in errors
+# they are meant for examining the errors and undesired blocks should be commented out temporarily to examine desired blocks 
 # the initial instance of this code produces an error
 for i in text.split():
     x = re.match(r"\bS\w+", i)
@@ -10,3 +12,17 @@ for i in text.split():
 
 # attribute is any property associated with objects
 # check attribute.py for further understanding
+
+# 'nonetype' object has no attribute 'xyz'
+# when trying to access None, it returns an unexpected output
+x = None
+print(x.insert) # this object has no attribute insert
+
+# another instance of nonetype errors
+def take(a):
+    if a < 0:
+        return a
+
+y = take(5) # y = None in this case
+print(y.func) # func attribute does not exist
+
