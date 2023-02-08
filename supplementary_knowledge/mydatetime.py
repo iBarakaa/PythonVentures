37ff4eb3d8b6d2d1f814f6f289742aeab0adfca3
iBarakaa <isaacbaraka1@gmail.com>
2023-02-08 10:00:55 +0300

    Understanding how to work with the timedelta object

--- supplementary_knowledge/mydatetime.py
+++ supplementary_knowledge/mydatetime.py
@@ -2,5 +2,14 @@
 # had initially name the file to "datetime.py" which caused me a circular import error upon calling the module in exe
 import datetime
 
-ldgsm = datetime.date(2001, 1, 17)
-print(ldgsm.day)
+ldgsm_birth = datetime.date(2001, 1, 17)
+print(ldgsm_birth)
+
+# timedelta object aids in increasing and decreasing dates
+# +ve no. increases
+# -ve no. decreases
+ldgsm_at_100days_old = datetime.timedelta(100)
+print(ldgsm_at_100days_old + ldgsm_birth)
+
+hundredDays_before_ldgsm_birth = datetime.timedelta(-100)
+print(hundredDays_before_ldgsm_birth + ldgsm_birth)
